d364f6fa0655fc00d34316bc9f948bd1e0a0508b
nshiff <nshiff@users.noreply.github.com>
2018-01-21 19:26:22 -0500

    basic function to get pivot subscript (last element)

--- Quicksort.py
+++ Quicksort.py
@@ -2,3 +2,6 @@ class Quicksort(object):
 
     def sort(self, list_to_sort):
         return list_to_sort
+
+    def get_pivot_subscript(self, list1):
+        return len(list1) - 1
--- TestQuicksort.py
+++ TestQuicksort.py
@@ -13,4 +13,12 @@ class TestQuicksort(TestCase):
         baby_list = [42]
         self.assertEqual(baby_list, self.qs.sort(baby_list))
 
+    def test_get_pivot_subscript(self):
+        list1 = [42]
+        list2 = [7, 9, 6, 4, 4, 2, 6, 7, 3, 4]
+        list3 = [2, 1, 3, 5]
+        self.assertEqual(0, self.qs.get_pivot_subscript(list1))
+        self.assertEqual(9, self.qs.get_pivot_subscript(list2))
+        self.assertEqual(3, self.qs.get_pivot_subscript(list3))
+
 
